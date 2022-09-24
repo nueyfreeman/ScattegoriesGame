8c958f1a2bb255a345a200dd3c6f038134b5f15c
nueyfreeman <nate.mcnaire@gmail.com>
2022-09-24 19:26:07 -0400

    Found root cause of .update issue and wrote reflection in comments

--- play_round.py
+++ play_round.py
@@ -6,17 +6,44 @@ def add_some(obj):
     obj.add_pt(random.randint(0, 5))
 
 
+def show_with_id(variable):
+    print(variable)
+    print(hex(id(variable)))
+
+
 def main():
+    x = 10
+    show_with_id(x)
+    y = 10
+    show_with_id(y)
+    y += 2
+    show_with_id(y)
+    x = 5
+    show_with_id(x)
+    x = [3, 4, 6]
+    show_with_id(x)
+    x.append(8)
+    show_with_id(x)
+    x = [3, 4, 5]
+    show_with_id(x)
+    list_test()
+
+
+def list_test():
     ps = []
+    abc = 'abc'
     for i in range(3):
         ps.append(user.Player(i))
+        var = {abc[i]: i}
         ps[i].add_pt(2)
-        #add_some(ps[i])
+        add_some(ps[i])
         ps[i].add_win()
-    ps[0].add_pt(3)
-    ps[2].add_win()
-    user.Player.wins = 3
+        # ps[i].answers.clear()
+        ps[i].answers.update(var)
+    ps[1].wins = 3
     print(f'{ps[0]}, {ps[2]}, {ps[1]}')
+    for i in range(3):
+        show_with_id(ps[i].answers)
 
 
 if __name__ == '__main__':
--- players.py
+++ players.py
@@ -6,17 +6,24 @@ This module will designate a class for individual players of a round.
 
 
 class Player:
-    def __init__(self, player_id):  # not necessary at this point but I like ability to distinguish obj by int
-        self.player_id = player_id
-
-    name = ''  # add double underscore to distinguish as private variable (add revisit encapsulation and aggregation)
     answers = {}
     points = 0
     total_points = 0
     wins = 0
 
+    def __init__(self, player_id):  # not necessary at this point, but I like ability to distinguish obj by int
+        self.player_id = player_id
+        self.name = self.pick_name()  # WORKS BUT... what's going on? I use the variable while defining the variable
+
+    def __str__(self):
+        return f'{self.name} ' \
+               f'(Round Points: {self.points}, Total Points: {self.total_points}, Wins: {self.wins})'
+
+    # name = ''  # add double underscore to distinguish as private variable (add revisit encapsulation and aggregation)
+
     def pick_name(self):
         self.name = input('Please choose a name: ')
+        return self.name
 
     def add_pt(self, count):
         self.points += count
@@ -35,21 +42,34 @@ class Player:
     def set_ans(self, final):
         self.answers = final
         """
+        SOLVED
+        
         self.answers.update(final) ruins score calculation (returning same score of the last player to go
         for every player...), why is that???
         
-        self.answers.update(final) -- makes what appears to be a class variable called by "obj.answers"
-        self.answers = final -- saves final as an instance variable called by "obj.answers"
+        self.answers.update(final) -- sets/resets what appears to be a class variable called by "obj.answers"
+        self.answers = final -- saves "final" as an instance variable called by "obj.answers"
         
-        problem seems to be that i've done all this without knowing class variables were a thing. I didn't
+        the problem seems to be that i've done all this without knowing class variables were a thing. I didn't
         have any problems, but now I'm finding that some of the variables i've used can actually act as
         class variables by accident, even though i've intended and successfully used them as instance
         variables up to this point
         
-        so I actually seem to be quite lacking in knowledge here and presently have no idea how my classes
-        will behave. essentially I have been getting lucky so far by designing a program assuming that
-        there is one kind of variable in a class, when actually there are multiple and I don't know how
-        to tell which kind i've created or used  
+        it turns out my problem was not only in the difference between instance variables and class variables
+        but also in not having a full grasp of the difference between mutable and immutable variables
+        
+        class variables can only be mutable (lists, sets, dictionaries) -- you can designate a class variable
+        and assign it an immutable type, but then it will still behave as an instance variable. so when I was
+        confused that my class variables, which i originally had designed as instance variables, were still
+        behaving as instance variables despite my mistake, it was because they were all of immutable types.
+        
+        except the "answer" variable which was a dictionary. so my program was working with " = final" because
+        i was actually reassigning to a brand new dictionary object each time. when i changed it to ".update"
+        it uncovered the mistake (which had actually been there the whole time) by actually using a class
+        variable properly by mutating the same dictionary object.
+        
+        will now rework my classes with proper variable instrumentation and make sure im using the proper
+        mutable methods for the relevant variables (which should optimize the thing i think?)
         """
 
     def get_ans(self):
